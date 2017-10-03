8a9c1ea256d5199e92c564a4407ebd6e363334f1
Vignesh S <vkkpp@hotmail.com>
2017-10-03 21:02:51 +0530

    added dynamic serving of mime types

--- app.js
+++ app.js
@@ -2,13 +2,36 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const helmet = require('helmet')
+const compression = require('compression')
+const url = require('url')
+const fs = require('fs')
 
 app.use(helmet())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(compression())
+app.use(express.static(path.join(__dirname, './public')))
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
 })
+
+app.get('/*.*', (req, res) => {
+  var options = url.parse(req.url, true)
+  var mime = Helper.getMime(options)
+  serveFile(res, options.pathname, mime)
+})
+
+function serveFile(res, pathName, mime) {
+  mime = mime || 'text/html'
+
+  fs.readFile(__dirname + '/' + pathName, (err, data) => {
+    if (err) {
+      res.writeHead(500, { "Content-Type": "text/plain" })
+      return res.end('Error loading ' + pathName + " with Error: " + err)
+    }
+    res.writeHead(200, { "Content-Type": mime })
+    res.end(data)
+  })
+}
 
 app.set('port', process.env.port || 8080)
 var server = app.listen(app.get('port'), (err) => {
